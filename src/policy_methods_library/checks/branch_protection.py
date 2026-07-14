f6a9f6ffe25f783c9600f8d61afa7dc00b358738
delterr <Hasan.T.Ahmad@ons.gov.uk>
2026-07-14 22:13:14 +0100

    Complete branch protection for protected field

--- src/policy_methods_library/checks/branch_protection.py
+++ src/policy_methods_library/checks/branch_protection.py
@@ -1,22 +1,14 @@
 """This module contains a check to verify that branch protection is enabled"""
-
 from policy_methods_library.github.clients import GitHubRestClient
+import requests
 
 
 def check_branch_protection(
     client: GitHubRestClient,
     repository_name: str,
+    branch_name: str
 ) -> dict:
-    """Check if a GitHub repository has branch protection enabled.
-
-    Args:
-        client: An instance of the GitHubRestClient to use for API calls. Required.
-        repository_name: The name of the repository to check. Required if data is not provided. Defaults to None.
-
-    Returns:
-        A dictionary with the result of the check, including 'result' (pass/fail/error), 'message', and 'details'.
-        Details contains the 'team_slug', 'maintainer_count', and 'maintainers' list.
-    """
+    """Check if a GitHub repository has branch protection enabled."""
 
     if client is None:
         return {
@@ -35,33 +27,88 @@ def check_branch_protection(
     try:
         response = client.make_request(
             "GET",
-            f"/repos/{client.owner}/{repository_name}/branches/main/protection",
+            f"/repos/{client.owner}/{repository_name}/branches/{branch_name}/protection",
         )
-
-        protection = response.json()
-
-        if protection.get("status") == 404:
+        data = response.json()
+        return {
+            "result": "pass",
+            "message": f"Branch protection is enabled for the '{branch_name}' branch of repository '{repository_name}'.",
+            "details": {
+                "required_status_checks": data.get("required_status_checks"),
+                "enforce_admins": data.get("enforce_admins", {}).get("enabled", False),
+                "required_pull_request_reviews": data.get("required_pull_request_reviews"),
+                "restrictions": data.get("restrictions"),
+            },
+        }
+    except requests.exceptions.HTTPError as e:
+        response = client.make_request(
+            "GET",
+            f"/repos/{client.owner}/{repository_name}/branches/{branch_name}"
+        ).json()
+        
+        if not response["protected"]:
             return {
                 "result": "fail",
-                "message": f"Branch protection is not enabled for the 'main' branch of repository '{repository_name}'.",
-                "details": {},
+                "message": f"Branch protection is not enabled for branch {branch_name}"
             }
         else:
+            response = client.make_request(
+                "GET",
+                f"/repos/{client.owner}/{repository_name}/rules/branches/{branch_name}"
+            ).json()
+
+            criteria = {
+                        "restrict_deletions": False,
+                        "review_before_merge": False,
+                    }
+
+            for rule in response:
+                match rule["type"]:
+                    case "deletion":
+                        criteria["restrict_deletions"] = True
+                    case "pull_request":
+                        require_code_owner_review = rule["parameters"]["require_code_owner_review"]
+                        required_approving_review_count = rule["parameters"]["required_approving_review_count"]
+
+                        if require_code_owner_review and required_approving_review_count >= 2:
+                            criteria["review_before_merge"] = True
+                    case _:
+                        pass
+            
+            if criteria["restrict_deletions"]:
+                return {
+                    "result": "fail",
+                    "message": "Branch deletions must be restricted",
+                    "details": {
+                        "Repository": repository_name,
+                        "Branch": branch_name,
+                    }
+                }
+            
+            if criteria["review_before_merge"]:
+                return {
+                    "result": "fail",
+                    "message": "Review before merge must be enabled",
+                    "details": {
+                        "Repository": repository_name,
+                        "Branch": branch_name,
+                    }
+                }
+
             return {
                 "result": "pass",
-                "message": f"Branch protection is enabled for the 'main' branch of repository '{repository_name}'.",
+                "message": "Branch is protected",
                 "details": {
-                    "required_status_checks": protection.get("required_status_checks"),
-                    "enforce_admins": protection.get("enforce_admins", {}).get(
-                        "enabled", False
-                    ),
-                    "required_pull_request_reviews": protection.get(
-                        "required_pull_request_reviews"
-                    ),
-                    "restrictions": protection.get("restrictions"),
-                },
+                    "Repository": repository_name,
+                    "Branch": branch_name,
+                }
             }
-
+        
+        return {
+            "result": "error",
+            "message": f"An error occurred while checking repository access: {str(e)}",
+            "details": {},
+        }
     except Exception as e:
         return {
             "result": "error",
